ce471d390e26c9bb6133ba99e6ec6564cbdf5c92
Vito Kortbeek <vito.kortbeek@gmail.com>
2016-10-28 15:50:02 +0200

    Adds multiple test checkers to script

--- testchecker.sh
+++ testchecker.sh
@@ -1,8 +1,36 @@
 #!/bin/sh
+# Tests the checks against non correct models to determine the validity of
+# the tests themselves.
 
-make tests | grep '^false$' &> /dev/null
-if [ $? == 0 ]; then
-    echo "A test failed!"
-else
-    echo "All tests passed"
-fi
+# Comment out SHOW_TEST to only show true or false (so leave out the test names)
+SHOW_TEST="TRUE"
+
+function showTest {
+    tmp="$1"
+    if [ "$SHOW_TEST" = "TRUE" ]; then
+        outp="${tmp//Running test: Verified_model\/Verification_files\//Test: }"
+        echo "${outp//".pbes"/}"
+    else
+        echo "$tmp" | egrep '^(false|true)$'
+    fi
+}
+
+DEAD_MODEL="Railway_crossing_dead"
+UNSAFE_MODEL="Railway_crossing_unsafe"
+EGREP_STR="(Running test:)|(^(false|true)$)"
+
+echo "Running the regular tests"
+output=$(make tests | egrep "$EGREP_STR")
+showTest "$output"
+echo
+
+echo "Running the dead tests"
+output=$(make MODEL_NAME=$DEAD_MODEL tests | egrep "$EGREP_STR")
+showTest "$output"
+echo
+
+echo "Running the unsafe tests"
+output=$(make MODEL_NAME=$UNSAFE_MODEL tests | egrep "$EGREP_STR")
+showTest "$output"
+echo
+
